6778eaecfac72d96994b7c094fdd91d04d7a2f4d
Shubham Tiwari <tiwarishubham5332@gmail.com>
2026-05-21 19:34:35 +0530

    config change

--- src/features/TeacherManagement/Pages/TeacherList.tsx
+++ src/features/TeacherManagement/Pages/TeacherList.tsx
@@ -14,7 +14,7 @@ import { SalaryStructureTable } from '../Components/SalaryStructureTable';
 import { PayslipManagement } from '../Components/PayslipManagement';
 import { EmployeeSettingsPanel } from '../Components/EmployeeSettingsPanel';
 import { Teacher, DEPARTMENTS } from '../Constants';
-import { useTeacherList } from '../hooks/useTeacherList';
+import { useTeacherList } from '../hooks/useTeacherList.ts';
 
 export function TeacherList() {
   const {
