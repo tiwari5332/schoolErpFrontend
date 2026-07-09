d9477c7276ac56afc540c964f84d567b5b449d31
Shubham Tiwari <tiwarishubham5332@gmail.com>
2026-07-09 22:15:00 +0530

    v++

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Main from './main.tsx'
 import './index.css'
 
-console.log('App version: 1.0.3 - Vercel Fix Deployment');
+console.log('App version: 1.0.4 - Vercel Fix Deployment');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
